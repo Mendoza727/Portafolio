197eee9fe4d661f9f534832773e1bff563106db8
Juan Camilo Mendoza Villegas <86323194+Mendoza727@users.noreply.github.com>
2023-01-13 23:20:20 -0500

    portafolio

---
+++ js-files/app.js
@@ -0,0 +1,289 @@
+/* IMPORTACIONES*/
+import { skillsListArray } from "./data/skills.js";
+
+
+/* VARIABLES */
+
+const skillsDom = document.getElementById('skill-div');
+const typed = new Typed('.typed', {
+    strings: [
+        '<i>Juan Camilo</i>', 
+        '<i class="text-success">Developer</i>'
+    ],
+    typeSpeed: 70,
+    backSpeed: 60,
+    backDelay: 2000,
+    startDelay: 1000,
+    loop: true,
+    cursorChar: '|',
+});
+
+var github = document.getElementById('content-github');
+var linkedin = document.getElementById('content-linkedin');
+var whatsapp = document.getElementById('content-whatsapp');
+
+/* FUNCIONES */
+skillsListArray.forEach(skill => {
+    const {nameSkill, img} = skill;
+    const skillContainerView = document.createElement('card');
+    skillContainerView.classList.add('card','col-sm-12','col-lg-1','card2');
+    const skillImageSrc = document.createElement('img');
+          skillImageSrc.classList.add('img-fluid');
+          skillImageSrc.src = img;
+          skillImageSrc.alt = `${nameSkill} Logo`;
+          skillImageSrc.style.height = '75%';
+    const skillName = document.createElement('p');
+    skillName.textContent = nameSkill;
+    skillName.classList.add('text-center','text-dark');
+    skillContainerView.appendChild(skillImageSrc);
+    skillContainerView.appendChild(skillName);
+    skillsDom.appendChild(skillContainerView);
+});
+
+// const portfolioContainer = document.querySelector('#portfolio-container');
+//   portfolioList.forEach(project => {
+//     const { name, img, project_url, repository_url, category } = project;
+//     const projectContainer = document.createElement('div');
+//       projectContainer.classList.add('col-lg-4', 'col-sm-6', 'portfolio-item', `filter-${category}`);
+//     const shadowContainer = document.createElement('div');
+//       shadowContainer.classList.add('portfolio-wrap', 'shadow', 'rounded-1');
+//     const projectTitle = document.createElement('h5');
+//       projectTitle.classList.add('portafolio-item-title');
+//       projectTitle.textContent = name;
+//     const projectImg = document.createElement('img');
+//       projectImg.src = img;
+//       projectImg.classList.add('img-fluid');
+//       projectImg.alt = `${name} Project Screenshot`;
+//     const linksContainer = document.createElement('div');
+//       linksContainer.classList.add('portfolio-links');
+//     const projectLink = document.createElement('a');
+//       projectLink.href = project_url;
+//       projectLink.target = '_blank';
+//       projectLink.title = 'View';
+//     const projectLinkIcon = document.createElement('i');
+//       projectLinkIcon.classList.add('bi', 'bi-box-arrow-up-right');
+//     const repositoryLink = document.createElement('a');
+//       repositoryLink.href = repository_url;
+//       repositoryLink.target = '_blank';
+//       repositoryLink.title = 'Repository';
+//     const repositoryLinkIcon = document.createElement('i');
+//       repositoryLinkIcon.classList.add('bi', 'bi-github');
+
+//     projectLink.appendChild(projectLinkIcon);
+//     repositoryLink.appendChild(repositoryLinkIcon);
+//     linksContainer.appendChild(projectLink);
+//     linksContainer.appendChild(repositoryLink);
+//     shadowContainer.appendChild(projectTitle);
+//     shadowContainer.appendChild(projectImg);
+//     shadowContainer.appendChild(linksContainer);
+//     projectContainer.appendChild(shadowContainer);
+//     portfolioContainer.appendChild(projectContainer);
+//   });
+
+/* mouse events */
+github.onmouseover = function() {
+  this.style.textDecoration = "underline";
+};
+
+github.onmouseout = function() {
+  this.style.textDecoration = "none";
+  this.style.color = "black";
+};
+
+linkedin.onmouseover = function() {
+  this.style.textDecoration = "underline";
+  this.style.color = 'blue';
+};
+
+linkedin.onmouseout = function() {
+  this.style.textDecoration = "none";
+  this.style.color = "black";
+};
+
+whatsapp.onmouseover = function() {
+  this.style.textDecoration = "underline";
+  this.style.color = 'green';
+};
+
+whatsapp.onmouseout = function() {
+  this.style.textDecoration = "none";
+  this.style.color = "black";
+};
+
+
+
+//scroll menu click
+var menuClickScrollContent = {
+
+    move : function (old, des, actual) {
+      easeInOutQuart = function (t) { return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t; };
+      actual += 1;
+      ease = easeInOutQuart(actual / 100);
+      delta = des - old;
+      delta *= ease;
+      delta += old;
+      window.scrollTo(0, delta);
+      if (actual < 100) {
+        window.requestAnimationFrame(function () {
+          menuClickScrollContent.move(old, des, actual);
+        });
+      }
+    },
+  
+    linkInit : function (e) {
+      e.preventDefault();
+      link = e.target.getAttribute("href").substr(1);
+      block = document.getElementById(link).offsetTop;
+      client = document.documentElement.scrollTop;
+  
+      menuClickScrollContent.move(client, block, 0);
+    },
+  
+    init : function () {
+      links = document.getElementsByTagName("a");
+      for (var i = 0; i < links.length; i++) {
+        link = links[i].getAttribute("href");
+        if (link.search("#") == 0 & link.substr(1) != "") {
+          links[i].onclick = menuClickScrollContent.linkInit;
+        }
+      }
+    }
+  };
+
+window.onload = menuClickScrollContent.init;
+
+//js function library ScrollReveal.js
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+    reset: false
+});
+
+ScrollReveal().reveal(".title", {
+    duration: 3000,
+    origin: "top",
+    distance: "400px",
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    rotate: {
+      x: 20,
+      z: -10
+    }
+});
+
+ScrollReveal().reveal(".slide-right", {
+    duration: 3000,
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out"
+});
+
+/* EVENTOS */
+
+// Código que se ejecutará una vez que el documento HTML ha sido cargado
+
+window.addEventListener("load", function() {
+    console.log("Todas las funciones del documento se han cargado sin errores :)");
+});
+
+
+
+
+/* PORTAFOLIO JS */
+
+window.addEventListener('load', () => {
+  let portfolioContainer = select('.portafolio-container');
+  if (portfolioContainer) {
+    let portfolioIsotope = new Isotope(portfolioContainer, {
+      itemSelector: '.portafolio-item',
+      filter: '.filter-css'
+    });
+    let portfolioFilters = select('#portafolio-filters li', true);
+
+    on('click', '#portafolio-filters li', function(e) {
+      e.preventDefault();
+      portfolioFilters.forEach(function(el) {
+        el.classList.remove('filter-active');
+      });
+      this.classList.add('filter-active');
+
+      portfolioIsotope.arrange({
+        filter: this.getAttribute('data-filter')
+      });
+      portfolioIsotope.on('arrangeComplete', function() {
+        AOS.refresh()
+      });
+    }, true);
+  }
+
+});
+
+/* light box */
+
+const portfolioLightbox = GLightbox({
+  selector: '.portafolio-lightbox'
+});
+
+/* slider */
+
+new Swiper('.portafolio-details-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+
+  
---
+++ js-files/data/skills.js
@@ -0,0 +1,57 @@
+export const skillsListArray = [
+    {
+        //JAVASCRIPT
+        nameSkill: "JavaScript",
+        img: "https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_960_720.png"
+    },
+    {   
+        //TYPESCRIPT
+        nameSkill: "TypeScript",
+        img: "https://cdn.worldvectorlogo.com/logos/typescript.svg"
+    },
+    {
+        //PHP   
+        nameSkill: "Php",
+        img: "https://cdn.pixabay.com/photo/2012/04/11/11/59/elephpant-27753_960_720.png"
+    },
+    {
+        //HTML
+        nameSkill: "Html",
+        img: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png"
+    },
+    {
+        //SCSS
+        nameSkill: "Scss",
+        img: "https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg"
+    },
+    {
+        //BOOSTRAP
+        nameSkill: "Boostrap",
+        img: "https://seeklogo.com/images/B/bootstrap-5-logo-85A1F11F4F-seeklogo.com.png"
+    },
+    {
+        //CSS
+        nameSkill: "Css",
+        img: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png"
+    },
+    {
+        //ANGULAR
+        nameSkill: "Angular",  
+        img: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg"
+    },
+    {
+        //REACT
+        nameSkill: "React",
+        img: "https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon.png"
+    },
+    {
+        //DATABASE
+        nameSkill: "MySql",
+        img: "https://w7.pngwing.com/pngs/924/1009/png-transparent-mysql-relational-database-management-system-logo-php-others-orange-logo-mysql-logo.png"
+    },
+    {
+        //NODE
+        nameSkill: "Node",
+        img: "https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399_960_720.png"
+    }
+];
